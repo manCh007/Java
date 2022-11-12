aa74eacc0d422257a26f88245ba80fb52c0a9c87
Manab Chatterjee <manab.formal@gmail.com>
2022-11-12 12:54:30 +0530

    Config Update
    Config Update

---
+++ With Spring Boot/SpringCloudArchitecture/ClientApplication/src/test/java/com/manCh007/clientApp/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manCh007.clientApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
