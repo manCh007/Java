013b165d3c6f6b27875c339dcc886e0c04937aa8
Manab Chatterjee <manab.formal@gmail.com>
2022-11-12 13:39:07 +0530

    Arrays
    Arrays

---
+++ DataStructures/ArraysExamples/src/main/java/com/manCh007/example/array/ArraysExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.manCh007.example.array;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraysExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArraysExamplesApplication.class, args);
+	}
+
+}
---
+++ DataStructures/ArraysExamples/src/test/java/com/manCh007/example/array/ArraysExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manCh007.example.array;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArraysExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
